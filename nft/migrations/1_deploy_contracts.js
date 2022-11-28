eac2c98a8a3be0b5f642e8021d7e3fa0c3b6864c
Siu Hwang <siuh0403@naver.com>
2022-11-28 23:33:04 +0900

    Delete nft directory

--- nft/migrations/1_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const Metafloris = artifacts.require("Metafloris");
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, Metafloris);
-  deployer.deploy(Metafloris);
-};
